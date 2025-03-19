d2219cb8a7f1860b1d96c959725b98764a62ec77
Omar-Tood <omarjibrilabdulkhadir@gmail.com>
2025-03-19 18:32:07 +0300

    📝Update: Increase spacing between logo and navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -47,23 +47,23 @@ export default function Navbar() {
         <div className="container-custom px-4 sm:px-6 lg:px-8" suppressHydrationWarning>
           <div className="flex items-center justify-between h-16 md:h-20" suppressHydrationWarning>
             {/* Logo */}
-            <Link href="/" className="flex items-center shrink-0">
+            <Link href="/" className="flex items-center shrink-0 mr-12 lg:mr-16">
               <div className="flex items-center">
                 <Image
                   src="/images/logo/logo-light.png"
-                  width={120}
-                  height={30}
+                  width={160}
+                  height={40}
                   alt="Soplang Logo"
-                  className="hidden dark:inline-block w-auto h-5 md:h-7"
+                  className="hidden dark:inline-block w-auto h-8 md:h-10"
                   priority
                   style={{ objectFit: 'contain' }}
                 />
                 <Image
                   src="/images/logo/logo-dark.png"
-                  width={120}
-                  height={30}
+                  width={160}
+                  height={40}
                   alt="Soplang Logo"
-                  className="inline-block dark:hidden w-auto h-5 md:h-7"
+                  className="inline-block dark:hidden w-auto h-8 md:h-10"
                   priority
                   style={{ objectFit: 'contain' }}
                 />
@@ -71,7 +71,7 @@ export default function Navbar() {
             </Link>
 
             {/* Desktop Navigation */}
-            <div className="hidden md:flex md:items-center md:space-x-6 lg:space-x-8" suppressHydrationWarning>
+            <div className="hidden md:flex md:items-center md:space-x-8 lg:space-x-10" suppressHydrationWarning>
               {navLinks.map((link) => (
                 <Link
                   key={link.path}
