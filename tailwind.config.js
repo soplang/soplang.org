b5211ac9cd6dabf95d858f41a69b71610eba8f38
Elkenzi <moelkenzi@gmail.com>
2025-08-08 19:56:43 +0300

    feat(ui): update on Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   theme: {
     screens: {
-      'xs': '480px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
       '2xl': '1536px',
     },
     extend: {
       colors: {
         primary: 'var(--primary-color)',
         secondary: 'var(--secondary-color)',
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
       },
       fontFamily: {
         sans: ['var(--font-roboto)', 'Roboto', 'sans-serif'],
       },
       spacing: {
-        '72': '18rem',
-        '84': '21rem',
-        '96': '24rem',
+        72: '18rem',
+        84: '21rem',
+        96: '24rem',
       },
     },
   },
-  plugins: [],
-} 
+  plugins: [require('tailwindcss-animate')],
+};
