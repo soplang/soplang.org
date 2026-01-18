c34ef05e6f36383d8a37123f4efa63be488bdaf1
Adam-Elmi <Adamcade123@gmail.com>
2026-01-19 02:24:43 +0300

    Updated sitemap with new routes

--- src/constants/sitemap.ts
+++ src/constants/sitemap.ts
@@ -1,73 +1,229 @@
 export const siteStructure = [
     {
-        title: "Getting Started",
-        links: [
-            { name: "Introduction", href: "/docs/getting-started" },
-            { name: "Quick Start Guide", href: "/docs/quick-start" },
-            { name: "Installation", href: "/docs/installation" },
-            { name: "First Program", href: "/docs/first-program" },
-        ],
-    },
-    {
-        title: "Downloads",
-        links: [
-            { name: "Latest Version", href: "/downloads/latest" },
-            { name: "All Releases", href: "/downloads" },
-            { name: "Windows", href: "/downloads/windows" },
-            { name: "macOS", href: "/downloads/macos" },
-            { name: "Linux", href: "/downloads/linux" },
-            { name: "Source Code", href: "/downloads/source" },
-        ],
-    },
-    {
-        title: "Documentation",
-        links: [
-            { name: "Language Reference", href: "/docs/language" },
-            { name: "Standard Library", href: "/docs/stdlib" },
-            { name: "API Reference", href: "/docs/api" },
-            { name: "Best Practices", href: "/docs/best-practices" },
-            { name: "Examples", href: "/docs/examples" },
-            { name: "Tutorials", href: "/docs/tutorials" },
-            { name: "FAQs", href: "/docs/faq" },
-            { name: "Extending Soplang", href: "/docs/extending" },
-        ],
-    },
-    {
-        title: "Community",
-        links: [
-            { name: "Community Home", href: "/community" },
-            { name: "Discord", href: "https://discord.gg/n296G4dd7x" },
-            { name: "GitHub", href: "https://github.com/soplang" },
+        "title": "About",
+        "links": [
             {
-                name: "Discussions",
-                href: "https://github.com/orgs/soplang/discussions",
+                "name": "Help",
+                "href": "/about/help"
             },
-            { name: "Contributing", href: "/contribute" },
-        ],
-    },
-    {
-        title: "Blog",
-        links: [
-            { name: "Blog Home", href: "/blog" },
             {
-                name: "Soplang 2.0",
-                href: "/blog/Soplang-2.0-Is-Almost-Here-A-Somali-first-Programming-Language-Nears-Major-Release",
+                "name": "About Home",
+                "href": "/about"
+            }
+        ]
+    },
+    {
+        "title": "Blog",
+        "links": [
+            {
+                "name": "Blog Home",
+                "href": "/blog"
+            }
+        ]
+    },
+    {
+        "title": "Community",
+        "links": [
+            {
+                "name": "Community Home",
+                "href": "/community"
+            }
+        ]
+    },
+    {
+        "title": "Contact",
+        "links": [
+            {
+                "name": "Contact Home",
+                "href": "/contact"
+            }
+        ]
+    },
+    {
+        "title": "Contribute",
+        "links": [
+            {
+                "name": "Security",
+                "href": "/contribute/security"
             },
-        ],
+            {
+                "name": "Contribute Home",
+                "href": "/contribute"
+            }
+        ]
     },
     {
-        title: "About",
-        links: [
-            { name: "About Soplang", href: "/about" },
-            { name: "Help", href: "/about/help" },
-        ],
+        "title": "Docs",
+        "links": [
+            {
+                "name": "Installing Soplang",
+                "href": "/docs/installation"
+            },
+            {
+                "name": "Getting Started with Soplang",
+                "href": "/docs/getting-started"
+            },
+            {
+                "name": "Soplang Syntax",
+                "href": "/docs/syntax-basics"
+            },
+            {
+                "name": "Arrays in Soplang",
+                "href": "/docs/arrays"
+            },
+            {
+                "name": "Built-in Functions in Soplang",
+                "href": "/docs/built-in-functions"
+            },
+            {
+                "name": "Conditional Statements",
+                "href": "/docs/conditional-statements"
+            },
+            {
+                "name": "Control Flow & Loops",
+                "href": "/docs/control-flow"
+            },
+            {
+                "name": "Soplang Data Types",
+                "href": "/docs/data-types"
+            },
+            {
+                "name": "Frequently Asked Questions",
+                "href": "/docs/faq"
+            },
+            {
+                "name": "Functions in Soplang",
+                "href": "/docs/functions"
+            },
+            {
+                "name": "Input and Output in Soplang",
+                "href": "/docs/input-and-output"
+            },
+            {
+                "name": "Soplang Keywords Reference",
+                "href": "/docs/keywords-reference"
+            },
+            {
+                "name": "Loops in Soplang",
+                "href": "/docs/loops"
+            },
+            {
+                "name": "Numbers in Soplang",
+                "href": "/docs/numbers"
+            },
+            {
+                "name": "Objects in Soplang",
+                "href": "/docs/objects"
+            },
+            {
+                "name": "Operators in Soplang",
+                "href": "/docs/operators"
+            },
+            {
+                "name": "Strings in Soplang",
+                "href": "/docs/strings"
+            },
+            {
+                "name": "Type Conversion in Soplang",
+                "href": "/docs/type-conversion"
+            },
+            {
+                "name": "Soplang Variables",
+                "href": "/docs/variables"
+            },
+            {
+                "name": "Docs Home",
+                "href": "/docs"
+            }
+        ]
     },
     {
-        title: "Legal",
-        links: [
-            { name: "Privacy Policy", href: "/privacy" },
-            { name: "Terms of Use", href: "/terms" },
-            { name: "Trademarks", href: "/trademarks" },
-        ],
+        "title": "Downloads",
+        "links": [
+            {
+                "name": "Downloads Home",
+                "href": "/downloads"
+            }
+        ]
     },
+    {
+        "title": "Examples",
+        "links": [
+            {
+                "name": "Hello World in Soplang",
+                "href": "/examples/hello-world"
+            },
+            {
+                "name": "Examples Home",
+                "href": "/examples"
+            }
+        ]
+    },
+    {
+        "title": "Faq",
+        "links": [
+            {
+                "name": "Faq Home",
+                "href": "/faq"
+            }
+        ]
+    },
+    {
+        "title": "Privacy",
+        "links": [
+            {
+                "name": "Privacy Home",
+                "href": "/privacy"
+            }
+        ]
+    },
+    {
+        "title": "Search",
+        "links": [
+            {
+                "name": "Search Home",
+                "href": "/search"
+            }
+        ]
+    },
+    {
+        "title": "Sitemap",
+        "links": [
+            {
+                "name": "Sitemap Home",
+                "href": "/sitemap"
+            }
+        ]
+    },
+    {
+        "title": "Terms",
+        "links": [
+            {
+                "name": "Terms Home",
+                "href": "/terms"
+            }
+        ]
+    },
+    {
+        "title": "Trademarks",
+        "links": [
+            {
+                "name": "Trademarks Home",
+                "href": "/trademarks"
+            }
+        ]
+    },
+    {
+        "title": "Tutorials",
+        "links": [
+            {
+                "name": "Introduction to Tutorials",
+                "href": "/tutorials/index"
+            },
+            {
+                "name": "Tutorials Home",
+                "href": "/tutorials"
+            }
+        ]
+    }
 ];
